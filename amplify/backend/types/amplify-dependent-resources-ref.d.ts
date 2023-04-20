09bf98a82819b9c159a325dc34201bbb8bb6278a
Suchat Tangjarukij <125326168+RadstalST@users.noreply.github.com>
2023-04-20 15:44:55 +1000

    added API

--- amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "quizzle": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
   "auth": {
     "quizzlea6890754": {
       "AppClientID": "string",
